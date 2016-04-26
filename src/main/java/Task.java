4e2cd2237f9014c5daa63713fd3235dc3d886496
Nadiya Yeroshkina and Patrick Wells <nadiya@>
2016-04-26 16:19:50 -0700

    update to do list by finding and saving object

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,58 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Task {
+  private static ArrayList<Task> instances = new ArrayList<Task>();
+
   private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private int mId;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getDescription() {
     return mDescription;
   }
+
+  public void completeTask(){
+    mCompleted = true;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static ArrayList<Task> all() {
+    return instances ;
+  }
+
+  public int getId(){
+    return mId;
+  }
+
+  public static Task find(int id){
+    try{
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e){
+      return null;
+    }
+  }
+
+  public static void clearTasksArray() {
+
+    //METHOD BELOW IS A BUILT IN METHOD
+    instances.clear();
+  }
+
 }
