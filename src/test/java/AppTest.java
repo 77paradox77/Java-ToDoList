873e8515ad734f0eab5eab426d818a0f5b209873
Nadiya Yeroshkina and Patrick Wells <patrick@>
2016-04-26 08:53:17 -0700

    add saving and retrieving tasks to app.java, tests are passing

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -16,20 +17,27 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
-  // 
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Welcome to the Vowel Game!");
-  // }
-  //
-  // @Test
-  // public void VowelsRemoved() {
-  //   goTo("http://localhost:4567");
-  //   fill("#vowelGame").with("Epicodus");
-  //   submit("button");
-  //   assertThat(pageSource()).contains("-p-c-d-s");
-  // }
 
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Task list!");
+  }
 
+  @Test
+  public void taskIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Mow the lawn");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your task has been saved.");
+  }
+
+  @Test
+  public void taskIsDisplayedTest() {
+   goTo("http://localhost:4567/");
+   fill("#description").with("Mow the lawn");
+   submit(".btn");
+   click("a", withText("Go Back"));
+   assertThat(pageSource()).contains("Mow the lawn");
+  }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -8,5 +8,9 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals(true, myTask instanceof Task);
   }
-
+  @Test
+  public void Task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
 }
